274340a304049baba23e137e2069d8fb63a3bc3d
Samuel Irenikase <samscode1529i@gmail.com>
2025-11-14 21:22:49 +0100

    game and win/lose display

---
+++ script.js
@@ -0,0 +1,76 @@
+
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+let playerDisplay = document.getElementById("playerDisplay")
+let computerDisplay = document.getElementById("computerDisplay")
+let resultDisplay = document.getElementById("resultDisplay")
+
+const choices = ["Rock", "Paper", "Scissors"]
+
+
+
+rock.addEventListener("click", function rocker(){
+    let playerChoice = "Rock"
+    playerDisplay.textContent = `Player: ${playerChoice}`
+
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    const computerChoice = choices[randomIndex]
+
+    console.log(randomIndex)
+    computerDisplay.textContent = `Computer: ${computerChoice}`
+
+    if (playerChoice === computerChoice){
+        resultDisplay.textContent = "It's a Tie Buddy"
+    }else if(playerChoice === "Rock" && computerChoice === "Paper"){
+        resultDisplay.textContent = "Better luck next time Broski"
+    }else if(playerChoice === "Rock" && computerChoice === "Scissors"){
+        resultDisplay.textContent = "Hehe I Win"
+    }
+})
+
+paper.addEventListener("click", function(){
+    let playerChoice = "Paper"
+    playerDisplay.textContent = `Player: ${playerChoice}`
+
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    const computerChoice = choices[randomIndex]
+
+    console.log(randomIndex)
+    computerDisplay.textContent = `Computer: ${computerChoice}`
+
+    if (playerChoice === computerChoice){
+        resultDisplay.textContent = "It's a Tie Buddy"
+        }else if(playerChoice === "Paper" && computerChoice === "Rock"){
+            resultDisplay.textContent = "Hehe I Win"
+        }else if(playerChoice === "Paper" && computerChoice === "Scissors"){
+            resultDisplay.textContent = "Damn It I'll beat u next time Clanker"
+        }
+})
+
+
+scissors.addEventListener("click", function(){
+    let playerChoice = "Scissors"
+    playerDisplay.textContent = `Player: ${playerChoice}`
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    const computerChoice = choices[randomIndex]
+
+
+    computerDisplay.textContent =`Computer:${computerChoice}`
+
+        if (playerChoice === computerChoice){
+            resultDisplay.textContent = "It's a Tie Buddy"
+            }else if(playerChoice === "Scissors" && computerChoice === "Rock"){
+                resultDisplay.textContent = "Damnit Computer"
+            }else if(playerChoice === "Scissors" && computerChoice === "Paper"){
+                resultDisplay.textContent = "Take This"
+            }
+})
+
+
+
+
+
+
+
+
